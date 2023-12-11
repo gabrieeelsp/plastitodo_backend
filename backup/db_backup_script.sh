75fc4a6a0acda7ff449aeac94caec4357816309a
gabrieeelsp <gabrieeelsp@gmail.com>
2023-12-11 11:27:01 -0300

    Se refactorizaron los archivos scripts de backup

--- backup/db_backup_script.sh
+++ backup/db_backup_script.sh
@@ -1,21 +1,25 @@
 #!/bin/sh
 
-BACKUPFOLDER=~/Dropbox/backup/
+DEPLOYFOLDER=/root/plastitodo/deploy-2023-12-08/plastitodo_backend
+BACKUPFOLDER=~/Dropbox/backup/db
 FILE=`date +"%Y-%m-%d-%H-%M"`-plastitodo_db.sql
 DBSERVER=localhost
 DBNAME=plastitodo_back_v5_db
 USER=root
 PASS=12345
 
-unalias rm     2> /dev/null
-rm ${FILE}     2> /dev/null
-rm ${FILE}.gz  2> /dev/null
+#unalias rm     2> /dev/null
+#rm ${FILE}     2> /dev/null
+#rm ${FILE}.gz  2> /dev/null
 
 #docker compose exec db /usr/bin/mysqldump --user=${USER} --password=${PASS} ${DATABASE} > ${FILE}
-docker compose exec db /usr/bin/mysqldump -u root --password=12345 ${DBNAME} > ${FILE}
+cd ${DEPLOYFOLDER}/backup
+docker compose exec db /usr/bin/mysqldump -u root -p12345 ${DBNAME} > ${FILE} 2>&1
 gzip $FILE
 
 mkdir -p $BACKUPFOLDER
-mv ${FILE}.gz ${BACKUPFOLDER}${FILE}.gz
+rm ${BACKUPFOLDER}/*
+#rm *db.sql.gz
+mv ${FILE}.gz ${BACKUPFOLDER}/${FILE}.gz
 echo "${FILE}.gz was created:"
-ls -l ${FILE}.gz
+#ls -l ${FILE}.gz
